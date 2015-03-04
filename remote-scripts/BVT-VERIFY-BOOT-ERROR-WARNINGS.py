f96bbc2a0c049e36a4c073e3507312779bbe37f7
Sivakanth <v-sirebb@microsoft.com>
2015-03-04 15:27:44 +0530

    Updated BVT test cases for CoreOS

--- remote-scripts/BVT-VERIFY-BOOT-ERROR-WARNINGS.py
+++ remote-scripts/BVT-VERIFY-BOOT-ERROR-WARNINGS.py
@@ -8,16 +8,19 @@ def RunTest():
     errors = Run("dmesg | grep -i error")
     warnings = Run("dmesg | grep -i warning")
     failures = Run("dmesg | grep -i fail")
-    if (not errors and not warnings and not failures) :
+    if (not errors and not warnings and not failures):
         RunLog.error('ERROR/WARNING/FAILURE are not present in kernel boot line.')
         ResultLog.info('PASS')
-        UpdateState("TestCompleted")
-    else :
-        RunLog.info('ERROR/WARNING/FAILURE are  present in kernel boot line.')
-        RunLog.info('Erros: ' + errors)
-        RunLog.info('warnings: ' + warnings)
-        RunLog.info('failures: ' + failures)
-        ResultLog.error('FAIL')
-        UpdateState("TestCompleted")
+    else:
+        if ("TSC calibration" in failures):
+            RunLog.info('Ignourable failure/warning are  present in kernel boot line.')
+            ResultLog.info('PASS')
+        else:	
+            RunLog.info('ERROR/WARNING/FAILURE are  present in kernel boot line.')
+            RunLog.info('Erros: ' + errors)
+            RunLog.info('warnings: ' + warnings)
+            RunLog.info('failures: ' + failures)
+            ResultLog.error('FAIL')
+    UpdateState("TestCompleted")
 
 RunTest()
--- remote-scripts/BVT-WA-VER-CHECK.py
+++ remote-scripts/BVT-WA-VER-CHECK.py
@@ -5,6 +5,13 @@ import sys
 import re
 from azuremodules import *
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-d', '--distro', help='Please mention which distro you are testing', required=True, type = str)
+
+args = parser.parse_args()
+distro = args.distro
+
 def RunTest(command):
     UpdateState("TestRunning")
     RunLog.info("Checking WALinuxAgent Version")
@@ -13,14 +20,17 @@ def RunTest(command):
     RegExp = re.compile(ExpectedVersionPattern)
 
     if (RegExp.match(output)) :
-                    RunLog.info('Waagent is in Latest Version .. - %s', output)
-                    ResultLog.info('PASS')
-                    UpdateState("TestCompleted")
+        RunLog.info('Waagent is in Latest Version .. - %s', output)
+        ResultLog.info('PASS')
+        UpdateState("TestCompleted")
     else :
-                    RunLog.error('Waagent version is differnt than required.')
-                    RunLog.error('Current version - %s', output)
-                    RunLog.error('Expected version pattern - %s', ExpectedVersionPattern)
-                    ResultLog.error('FAIL')
-                    UpdateState("TestCompleted")
+        RunLog.error('Waagent version is differnt than required.')
+        RunLog.error('Current version - %s', output)
+        RunLog.error('Expected version pattern - %s', ExpectedVersionPattern)
+        ResultLog.error('FAIL')
+        UpdateState("TestCompleted")
 
-RunTest("/usr/sbin/waagent --version")
+if(distro == "COREOS"):
+    RunTest("waagent --version")
+else:
+    RunTest("/usr/sbin/waagent --version")
