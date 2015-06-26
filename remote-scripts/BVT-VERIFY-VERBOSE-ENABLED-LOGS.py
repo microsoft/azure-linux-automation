48214b19805e937861d4b72445e115b35771e77e
Nidylei <v-feile@microsoft.com>
2015-06-26 15:07:30 +0800

    1.slight adjust the test order 2.fix some test issues against few distros

--- remote-scripts/BVT-VERIFY-VERBOSE-ENABLED-LOGS.py
+++ remote-scripts/BVT-VERIFY-VERBOSE-ENABLED-LOGS.py
@@ -7,20 +7,29 @@ import argparse
 import sys
 import time
 import platform
+import os
+#for error checking
+parser = argparse.ArgumentParser()
 
-def RunTest():
+parser.add_argument('-e', '--expected', help='specify expected value', required=True)
+
+args = parser.parse_args()
+#if no value specified then stop
+expectedValue = "1"
+def RunTest(expectedvalue):
     UpdateState("TestRunning")
     RunLog.info("Checking log waagent.log...")
-    output = Run("grep -i 'iptables -I INPUT -p udp --dport' /var/log/waagent* | wc -l | tr -d '\n'")
-    if not (output == "0") :
-        RunLog.info('The log file contains the verbose logs')
+    temp = Run("grep -i 'iptables -I INPUT -p udp --dport' /var/log/waagent* | wc -l | tr -d '\n'")
+    output = temp
+        
+    if (expectedvalue == output) :
+        RunLog.info('The log file contains the expected value')
         ResultLog.info('PASS')
         UpdateState("TestCompleted")
     else :
-        RunLog.error('Verify waagent.log fail, the log file does not contain the verbose logs')
+        RunLog.error('Verify waagent.log fail. Current value : %s Expected value : %s' % (output, expectedvalue))
         ResultLog.error('FAIL')
         UpdateState("TestCompleted")
-
 def Restartwaagent():
     distro = platform.dist()
     if (distro[0] == "CoreOS") :
@@ -31,12 +40,12 @@ def Restartwaagent():
     result = Run("echo 'Redhat.Redhat.777' | sudo -S find / -name systemctl |wc -l | tr -d '\n'")    
     if (distro[0] == "Ubuntu") :
         Run("echo 'Redhat.Redhat.777' | sudo -S service walinuxagent restart")
-    else :  
+    else :
+#for distro: centos6,oracle6 it will be hung when call Run,so use os.system() to replace it
         if (result == "0") :
-            Run("echo 'Redhat.Redhat.777' | sudo -S service waagent restart")
+            os.system("echo 'Redhat.Redhat.777' | sudo -S service waagent restart")
         else :
-            Run("echo 'Redhat.Redhat.777' | sudo -S systemctl restart waagent")
-    time.sleep(60)
-
+            os.system("echo 'Redhat.Redhat.777' | sudo -S systemctl restart waagent")
 Restartwaagent()
-RunTest()
+time.sleep(60)
+RunTest(expectedValue)
