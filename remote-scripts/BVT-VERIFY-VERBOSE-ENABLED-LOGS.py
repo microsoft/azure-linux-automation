ea9b1464eac523a1f4b5ea824b31bad79b8426dd
Sivakanth <v-sirebb@microsoft.com>
2015-07-06 13:02:41 +0530

    Minor fixes

--- remote-scripts/BVT-VERIFY-VERBOSE-ENABLED-LOGS.py
+++ remote-scripts/BVT-VERIFY-VERBOSE-ENABLED-LOGS.py
@@ -2,12 +2,18 @@
 
 from azuremodules import *
 
-
 import argparse
 import sys
 import time
 import platform
 import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-p', '--passwd', help='specify password of vm', required=True)
+args = parser.parse_args()
+
+passwd = args.passwd
 
 def RunTest():
     UpdateState("TestRunning")
@@ -25,18 +31,18 @@ def RunTest():
 def Restartwaagent():
     distro = platform.dist()
     if (distro[0] == "CoreOS") :
-        Run("echo 'Redhat.Redhat.777' | sudo -S sed -i s/Logs.Verbose=n/Logs.Verbose=y/g  /usr/share/oem/waagent.conf")
+        Run("echo '"+passwd+"' | sudo -S sed -i s/Logs.Verbose=n/Logs.Verbose=y/g  /usr/share/oem/waagent.conf")
     else :
-        Run("echo 'Redhat.Redhat.777' | sudo -S sed -i s/Logs.Verbose=n/Logs.Verbose=y/g  /etc/waagent.conf")
+        Run("echo '"+passwd+"' | sudo -S sed -i s/Logs.Verbose=n/Logs.Verbose=y/g  /etc/waagent.conf")
     RunLog.info("Restart waagent service...")
-    result = Run("echo 'Redhat.Redhat.777' | sudo -S find / -name systemctl |wc -l | tr -d '\n'")    
+    result = Run("echo '"+passwd+"' | sudo -S find / -name systemctl |wc -l | tr -d '\n'")    
     if (distro[0] == "Ubuntu") :
-        Run("echo 'Redhat.Redhat.777' | sudo -S service walinuxagent restart")
+        Run("echo '"+passwd+"' | sudo -S service walinuxagent restart")
     else :  
         if (result == "0") :
-            os.system("echo 'Redhat.Redhat.777' | sudo -S service waagent restart")
+            os.system("echo '"+passwd+"' | sudo -S service waagent restart")
         else :
-            os.system("echo 'Redhat.Redhat.777' | sudo -S systemctl restart waagent")
+            os.system("echo '"+passwd+"' | sudo -S systemctl restart waagent")
     time.sleep(60)
 
 Restartwaagent()
