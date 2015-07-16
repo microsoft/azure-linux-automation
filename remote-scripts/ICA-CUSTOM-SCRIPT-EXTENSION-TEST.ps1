4e66ec7c07abd3bc3b21b9009131ad15f1b21bca
Shital Savekar <iamshital@live.in>
2015-07-16 15:42:42 +0530

    Indentation Fixes.

--- remote-scripts/ICA-CUSTOM-SCRIPT-EXTENSION-TEST.ps1
+++ remote-scripts/ICA-CUSTOM-SCRIPT-EXTENSION-TEST.ps1
@@ -20,46 +20,46 @@ if ($isDeployed)
 		$hs1ServiceUrl = $hs1vm1.DNSName
 		$hs1ServiceUrl = $hs1ServiceUrl.Replace("http://","")
 		$hs1ServiceUrl = $hs1ServiceUrl.Replace("/","")
-        $lsOutput = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "ls /var/log/" -runAsSudo
-        LogMsg -msg $lsOutput -LinuxConsoleOuput
+		$lsOutput = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "ls /var/log/" -runAsSudo
+		LogMsg -msg $lsOutput -LinuxConsoleOuput
 		$out = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "tar -cvzf /home/$user/AzureExtensionLogs.tar.gz /var/log/azure/*" -runAsSudo
-        RemoteCopy -download -downloadFrom $hs1VIP -files "/home/$user/AzureExtensionLogs.tar.gz" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
-        $varLogFolder = "/var/log"
-        $lsOut = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "ls -lR $varLogFolder"
-        $LogFilesPaths = ""
-        $LogFiles = ""
-        $folderToSearch = "/var/log/azure"
-        foreach ($line in $lsOut.Split("`n") )
-        {
-            if ($line -imatch $varLogFolder)
-            {
-                $currentFolder = $line.Replace(":","")
-            }
-            if ( ( ($line.Split(" ")[0][0])  -eq "-" ) -and ($currentFolder -imatch $folderToSearch) )
-            {
-                if ($LogFilesPaths)
-                {
-                    $LogFilesPaths += "," + $currentFolder + "/" + $line.Split(" ")[8]
-                    $LogFiles += "," + $line.Split(" ")[8]
-                }
-                else
-                {
-                    $LogFilesPaths = $currentFolder + "/" + $line.Split(" ")[8]
-                    $LogFiles += $line.Split(" ")[8]
-                }
-            }
-        }
-        RemoteCopy -download -downloadFrom $hs1VIP -files $LogFilesPaths -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
-        RemoteCopy -download -downloadFrom $hs1VIP -files "/var/log/waagent.log" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
-        if ($lsOutput -imatch "CustomExtensionSuccessful")
-        {
-            $testResult = "PASS"
-        }
-        else
-        {
-            $testResult = "FAIL"
-            LogMsg "Please check logs."
-        }
+		RemoteCopy -download -downloadFrom $hs1VIP -files "/home/$user/AzureExtensionLogs.tar.gz" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
+		$varLogFolder = "/var/log"
+		$lsOut = RunLinuxCmd -username $user -password $password -ip $hs1VIP -port $hs1vm1sshport -command "ls -lR $varLogFolder"
+		$LogFilesPaths = ""
+		$LogFiles = ""
+		$folderToSearch = "/var/log/azure"
+		foreach ($line in $lsOut.Split("`n") )
+		{
+			if ($line -imatch $varLogFolder)
+			{
+				$currentFolder = $line.Replace(":","")
+			}
+			if ( ( ($line.Split(" ")[0][0])  -eq "-" ) -and ($currentFolder -imatch $folderToSearch) )
+			{
+				if ($LogFilesPaths)
+				{
+					$LogFilesPaths += "," + $currentFolder + "/" + $line.Split(" ")[8]
+					$LogFiles += "," + $line.Split(" ")[8]
+				}
+				else
+				{
+					$LogFilesPaths = $currentFolder + "/" + $line.Split(" ")[8]
+					$LogFiles += $line.Split(" ")[8]
+				}
+			}
+		}
+		RemoteCopy -download -downloadFrom $hs1VIP -files $LogFilesPaths -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
+		RemoteCopy -download -downloadFrom $hs1VIP -files "/var/log/waagent.log" -downloadTo $LogDir -port $hs1vm1sshport -username $user -password $password
+		if ($lsOutput -imatch "CustomExtensionSuccessful")
+		{
+			$testResult = "PASS"
+		}
+		else
+		{
+			$testResult = "FAIL"
+			LogMsg "Please check logs."
+		}
 		LogMsg "Test result : $testResult"
 	}
 
