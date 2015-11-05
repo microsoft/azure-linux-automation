b078dd6b58a6bb8edcde8bd4632d09904036f6ac
Nidylei <v-feile@microsoft.com>
2015-11-05 16:51:46 +0800

    minor fix

--- remote-scripts/BVT-WA-PROC-CHECK.py
+++ remote-scripts/BVT-WA-PROC-CHECK.py
@@ -5,7 +5,7 @@ from azuremodules import *
 def RunTest(command):
     UpdateState("TestRunning")
     RunLog.info("Checking WALinuxAgent in running processes")
-    temp = RunUpdate(command)
+    temp = Run(command)
     timeout = 0
     output = temp
     if ("waagent" in output) :
@@ -17,4 +17,4 @@ def RunTest(command):
                     ResultLog.Error('FAIL')
                     UpdateState("TestCompleted")
         
-RunTest("ps -ef")
+RunTest("ps -ef | grep waagent | grep -v 'grep waagent'")
