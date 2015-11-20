41fca8e563c8d5371aef5040dac7e5dc8128b814
Shital Savekar <iamshital@live.in>
2015-11-20 19:20:27 +0530

    Small Fix in checkKvpPoolZero.sh

--- remote-scripts/checkKvpPoolZero.sh
+++ remote-scripts/checkKvpPoolZero.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "`cat /var/lib/hyperv/.kvp_pool_0`" =~ "NdDriverVersion" ]]; then
+if [[ "`cat /var/lib/hyperv/.kvp_pool_0`" =~ NdDriverVersion.*142 ]]; then
     zypper --non-interactive --no-gpg-checks update kernel*
 else
     echo NdDriverVersion_String_Not_Found
