169f5e1b4c8a533b071918c8a54f1c808dc561f1
weiil <v-lii@microsoft.com>
2016-01-22 10:44:00 +0800

    py3: fix for start-client.py

--- remote-scripts/start-client.py
+++ remote-scripts/start-client.py
@@ -58,7 +58,7 @@ def RunTest(client):
 		Run('echo "ProcessRunning" >> iperf-client.txt')
 		Run('echo "Waiting for 60 secs to let iperf process finish" >> iperf-client.txt')
 		status = isProcessRunning('iperf -c')
-		if cmp(status, "True")==0:
+		if status == "True":
 			Run('echo "ProcessRunning even after 60 secs delay" >> iperf-client.txt')
 		else:
 			Run('echo iperf process finished after extra wait of 60 secs >>iperf-client.txt')
