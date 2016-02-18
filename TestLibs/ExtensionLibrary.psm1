e45e6b199db7be99e3cf6d47b4ebaafa360690a3
Shital Savekar <iamshital@live.in>
2016-02-19 00:21:22 +0530

    Updated extension Library
    Added function to set linux extension [ASM and ARM]

--- TestLibs/ExtensionLibrary.psm1
+++ TestLibs/ExtensionLibrary.psm1
@@ -54,14 +54,21 @@
 	return $retValue
 }
 
-Function DownloadExtensionLogFilesFromVarLog ($LogFilesPaths, $ExtensionName, $vmData)
+Function DownloadExtensionLogFilesFromVarLog ($LogFilesPaths, $ExtensionName, $vmData, [switch] $deleteAfterDownload)
 {
 	foreach ($file in $LogFilesPaths.Split(","))
 	{
 		$fileName = $file.Split("/")[$file.Split("/").Count -1]
 		if ( $file -imatch $ExtensionName )
 		{
-			$out = RunLinuxCmd -username $user -password $password -ip $vmData.PublicIP -port $vmData.SSHPort -command "cat $file > $fileName" -runAsSudo
+			if ( $deleteAfterDownload )
+			{
+				$out = RunLinuxCmd -username $user -password $password -ip $vmData.PublicIP -port $vmData.SSHPort -command "cat $file > $fileName && rm -rf $file" -runAsSudo
+			}
+			else
+			{
+				$out = RunLinuxCmd -username $user -password $password -ip $vmData.PublicIP -port $vmData.SSHPort -command "cat $file > $fileName" -runAsSudo
+			}
 			RemoteCopy -download -downloadFrom $vmData.PublicIP -files $fileName -downloadTo $LogDir -port $vmData.SSHPort -username $user -password $password
 		}
 		else
@@ -89,7 +96,7 @@ Function GetExtensionStatusFromStatusFile ( $statusFilePaths, $ExtensionName, $v
 					$statusFileNotFound = $false
 					$out = RunLinuxCmd -username $user -password $password -ip $vmData.PublicIP -port $vmData.SSHPort -command "cat $file > $fileName" -runAsSudo
 					RemoteCopy -download -downloadFrom $vmData.PublicIP -files $fileName -downloadTo $LogDir -port $vmData.SSHPort -username $user -password $password
-					$statusFile = Get-Content -Path "$LogDir\$fileName"
+					$statusFile = [string](Get-Content -Path "$LogDir\$fileName")
 					$extensionVarLibStatus = ConvertFrom-Json -InputObject $statusFile
 					if ( $extensionVarLibStatus.Status.status -eq "success" )
 					{
@@ -97,7 +104,7 @@ Function GetExtensionStatusFromStatusFile ( $statusFilePaths, $ExtensionName, $v
 					}
 					else
 					{
-						LogMsg "$fileName reported status : $($extensionVarLibStatus.Status.status)"
+						LogErr "$fileName reported status : $($extensionVarLibStatus.Status.status)"
 						$extensionErrorCount += 1
 					}
 					if ( $extensionVarLibStatus.Status.code -eq 0 )
@@ -107,7 +114,8 @@ Function GetExtensionStatusFromStatusFile ( $statusFilePaths, $ExtensionName, $v
 					else
 					{
 						LogErr "$fileName reported code : $($extensionVarLibStatus.Status.code)"
-						$extensionErrorCount += 1
+						LogMsg "Skipping this error because 'code' report is optional."
+						#$extensionErrorCount += 1
 					}
 				}
 				else
@@ -144,4 +152,155 @@ Function GetExtensionStatusFromStatusFile ( $statusFilePaths, $ExtensionName, $v
 	}
 	while (($retryCount -le $maxRetryCount) -and $waitForExtension )
 	return $retValue
+}
+
+Function SetAzureVMExtension ( $publicConfigString, $privateConfigString, $ExtensionName, $ExtensionVersion, $LatestExtensionVersion, $Publisher, $vmData, $maxRetryCount = 20, $retryIntervalInSeconds=10)
+{
+	$retryCount = 1
+	$waitForExtension = $true
+	$retValue = $false
+	if (!$privateConfigString -and !$publicConfigString )
+	{
+		Throw "Neither Public Configuration nor Private Configuration provided. Aborting this test."
+	}
+	do
+	{
+		LogMsg "attempt [$retryCount/$maxRetryCount] : Setting $ExtensionName for $($vmData.RoleName) ..."
+
+		if ( $UseAzureResourceManager )
+		{
+			$RGName = $vmData.ResourceGroupName
+			$VMName = $vmData.RoleName
+			$Location = (Get-AzureResourceGroup -Name $RGName).Location
+			if ( $publicConfigString -and $privateConfigString )
+			{
+				LogMsg "Public Config : $publicConfigString"
+				LogMsg "Private Config : $privateConfigString"
+				$ExtStatus = Set-AzureVMExtension -ResourceGroupName $RGName -VMName $VMName -Location $Location -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionName -TypeHandlerVersion $LatestExtensionVersion -Settingstring $publicConfigString -ProtectedSettingString $privateConfigString -Verbose
+			}
+			else
+			{
+				if ($publicConfigString)
+				{
+					LogMsg "Public Config : $publicConfigString"
+					$ExtStatus = Set-AzureVMExtension -ResourceGroupName $RGName -VMName $VMName -Location $Location -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionName -TypeHandlerVersion $LatestExtensionVersion -Settingstring $publicConfigString -Verbose
+				}
+				if ($privateConfigString )
+				{
+					LogMsg "Private Config : $privateConfigString"
+					$ExtStatus = Set-AzureVMExtension -ResourceGroupName $RGName -VMName $VMName -Location $Location -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionName -TypeHandlerVersion $LatestExtensionVersion -ProtectedSettingString $privateConfigString -Verbose
+				}
+			}
+			if ( $ExtStatus.Status -eq "Succeeded" )
+			{
+				$retValue = $true
+				$waitForExtension = $false
+			}
+			else
+			{
+				WaitFor -Seconds 30
+			}			
+
+		}
+		else
+		{
+
+			if ( $publicConfigString -and $privateConfigString )
+			{
+				LogMsg "Public Config : $publicConfigString"
+				LogMsg "Private Config : $privateConfigString"
+				$ExtStatus = Get-AzureVM -ServiceName $vmData.ServiceName -Name $vmData.RoleName | Set-AzureVMExtension -ExtensionName $ExtensionName -Publisher $Publisher -Version $ExtensionVersion -PrivateConfiguration $privateConfigString -PublicConfiguration $publicConfigString | Update-AzureVM -Verbose
+			}
+			else
+			{
+				if ($publicConfigString)
+				{
+					LogMsg "Public Config : $publicConfigString"
+					$ExtStatus = Get-AzureVM -ServiceName $vmData.ServiceName -Name $vmData.RoleName | Set-AzureVMExtension -ExtensionName $ExtensionName  -Publisher $Publisher -Version $ExtensionVersion -PublicConfiguration $publicConfigString | Update-AzureVM -Verbose
+				}
+				if ($privateConfigString )
+				{
+					LogMsg "Private Config : $privateConfigString"
+					$ExtStatus = Get-AzureVM -ServiceName $vmData.ServiceName -Name $vmData.RoleName | Set-AzureVMExtension -ExtensionName $ExtensionName -Publisher $Publisher -Version $ExtensionVersion -PrivateConfiguration $privateConfigString | Update-AzureVM -Verbose
+				}
+			}
+
+			if ( $ExtStatus.OperationStatus -eq "Succeeded" )
+			{
+				$retValue = $true
+				$waitForExtension = $false
+			}
+			else
+			{
+				WaitFor -Seconds 30
+			}
+		}
+
+		$retryCount += 1
+		if ( ($retryCount -le $maxRetryCount) -and $waitForExtension )
+		{
+			LogMsg "Retrying... $($maxRetryCount-$retryCount) attempts left..."
+		}
+		elseif ($waitForExtension)
+		{
+			LogMsg "Retry Attempts exhausted."
+		}
+	}
+	while (($retryCount -le $maxRetryCount) -and $waitForExtension )
+
+	return $retValue
+}
+
+Function GetStatusFileNameToVerfiy ($vmData, $expectedExtensionName, [switch]$upcoming, $maxRetryCount = 20, $retryInterval = 10)
+{
+	$statusFileCounter = 0
+	$waitForStatusFile = $true
+	$retryCount = 0
+	LogMsg "Getting current *.status file info..."
+	do
+		{
+		$retryCount += 1		
+		LogMsg "attempt [$retryCount/$maxRetryCount] : Setting $ExtensionName for $($vmData.RoleName) ..."
+		$currentExtFiles = GetFilePathsFromLinuxFolder -folderToSearch "/var/lib/waagent" -IpAddress $vmData.PublicIP -SSHPort $vmData.SSHPort -username $user -password $password -maxRetryCount 5
+		foreach ($line in $currentExtFiles[0].Split(","))
+		{
+			$tempFileName = $($line.Split('/')[ $line.Split('/').Count - 1 ])
+			if ( ($tempFileName -imatch "\d.status") -and ($line -imatch $expectedExtensionName))
+			{
+				LogMsg "Found : $line."
+				$statusFileCounter += 1 
+			}
+		}
+		if ( $upcoming )
+		{
+			if ($statusFileCounter -eq 0)
+			{
+				LogMsg "No any previous *.status file found. Hence setting expected status file as $statusFileCounter.status"
+				$statusFileToVerfiy = "$statusFileCounter.status"
+			}
+			else
+			{
+				LogMsg "Hence setting expected status file as $statusFileCounter.status"
+				$statusFileToVerfiy = "$statusFileCounter.status"
+			}
+			$waitForStatusFile = $false
+		}
+		else
+		{
+			if ($statusFileCounter -eq 0)
+			{
+				LogMsg "No any previous *.status file found."
+				WaitFor -seconds 10
+			}
+			else
+			{
+				$waitForStatusFile = $false
+				LogMsg "Hence setting status file as $($statusFileCounter-1).status"
+				$statusFileToVerfiy = "$($statusFileCounter-1).status"
+			}
+		}
+	}
+	while (($retryCount -le $maxRetryCount) -and $waitForStatusFile )
+	
+	return $statusFileToVerfiy
 }
