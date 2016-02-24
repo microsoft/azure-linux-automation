67da6828b7d652973efe4a0b46ff4f9b10c52916
SRIKKANTH <m.srikanth.m@gmail.com>
2016-02-24 23:19:25 +0530

    Added Host's version, VM's memory info collection logic

--- remote-scripts/generate_csvs.sh
+++ remote-scripts/generate_csvs.sh
@@ -5,69 +5,83 @@
 # Email	: v-srm@microsoft.com
 #
 format_bites(){
-temp_size=$1
-temp_units="Gbps"
-temp_size=`echo $temp_size 1000| awk '{printf "%.3f \n", $1/$2}'`
-temp_size=`echo $temp_size 1000| awk '{printf "%.3f \n", $1/$2}'`
-echo $temp_size,$temp_units
+	temp_size=$1
+	temp_units="Gbps"
+	temp_size=`echo $temp_size 1000| awk '{printf "%.3f \n", $1/$2}'`
+	temp_size=`echo $temp_size 1000| awk '{printf "%.3f \n", $1/$2}'`
+	echo $temp_size,$temp_units
 }
+
 gen_csv(){
-input_file=$1
-number_of_connections=$2
+	input_file=$1
+	number_of_connections=$2
 
-output_file=`echo $input_file |sed s/\.log$/\.csv/`
-top_array_file=`echo $output_file |sed s/sar/top/`
-conn_array_file=`echo $output_file |sed s/sar/connections/`
-cpu_vmstat_file=`echo $input_file |sed s/sar/vmstat/`
+	output_file=`echo $input_file |sed s/\.log$/\.csv/`
+	top_array_file=`echo $output_file |sed s/sar/top/`
+	conn_array_file=`echo $output_file |sed s/sar/connections/`
+	cpu_vmstat_file=`echo $input_file |sed s/sar/vmstat/`
 
-rx_array_file=$input_file-rxkBps.log
-tx_array_file=$input_file-txkBps.log
-cpu_vmstat_array_file=$input_file-cpu-vmstat.log
+	rx_array_file=$input_file-rxkBps.log
+	tx_array_file=$input_file-txkBps.log
+	cpu_vmstat_array_file=$input_file-cpu-vmstat.log
 
-cat $cpu_vmstat_file | grep -v [a-z]| awk '{print 100 - $15}' > $cpu_vmstat_array_file 
-cat $input_file | grep eth0 | awk '{print $6}' > $rx_array_file
-cat $input_file | grep eth0 | awk '{print $7}' > $tx_array_file
+	cat $cpu_vmstat_file | grep -v [a-z]| awk '{print 100 - $15}' > $cpu_vmstat_array_file 
+	cat $input_file | grep eth0 | awk '{print $6}' > $rx_array_file
+	cat $input_file | grep eth0 | awk '{print $7}' > $tx_array_file
 
-rx_array=(`cat $rx_array_file`)
-tx_array=(`cat $tx_array_file`)
-top_array=(`cat $top_array_file`)
-conn_array=(`cat $conn_array_file`)
-cpu_array=(`cat $cpu_vmstat_array_file`)
-length=$((${#rx_array[@]}-1))
-count=0
-sum=0
-rx_sum=0
-tx_sum=0
+	rx_array=(`cat $rx_array_file`)
+	tx_array=(`cat $tx_array_file`)
+	top_array=(`cat $top_array_file`)
+	conn_array=(`cat $conn_array_file`)
+	cpu_array=(`cat $cpu_vmstat_array_file`)
 
-echo Rx Throughput,Units,TxThroughput,Units,Total,Units,,Time,ActiveConnection,,Time,CPU Usage > $output_file
+	length=$((${#rx_array[@]}-1))
+	count=0
+	sum=0
+	rx_sum=0
+	tx_sum=0
 
+	res_kernel_version=(`cat $input_file | grep "Linux.*x86_64.*GNU"| awk '{print $3}'`)
+	res_total_cpu_cores=(`cat $input_file | grep "Number of CPU cores" | awk '{print $5}'`)
+	res_LIS_version=(`cat $input_file | grep "^version:"| awk '{print $2}'`)
+	res_Host_version=(`cat $input_file | grep "Host Build Version" | awk '{print $4}'`)
+	res_total_memory=(`cat $input_file |grep "^Memory" | awk '{print $2}'`)
+	
+	echo "" > $output_file
+	echo ",Kernel version,"$res_kernel_version >> $output_file
+	echo ",Total CPU cores,"$res_total_cpu_cores >> $output_file
+	echo ",Memory,"$res_total_memory >> $output_file
+	echo ",LIS Version,"$res_LIS_version >> $output_file
+	echo ",Host Version,"$res_Host_version >> $output_file
+	echo "" >> $output_file
 
-for i in `seq 1 $length`;
-do
-Tx_bits=`echo 8 ${tx_array[count]}| awk '{printf "%.3f \n", $1*$2}'`
-Rx_bits=`echo 8 ${rx_array[count]}| awk '{printf "%.3f \n", $1*$2}'`
+	echo Rx Throughput,Units,TxThroughput,Units,Total,Units,,Time,ActiveConnection,,Time,CPU Usage >> $output_file
 
-Tx_data=`format_bites $Tx_bits`
-Rx_data=`format_bites $Rx_bits`
-Total_bits=`echo $Tx_bits $Rx_bits| awk '{printf "%.3f \n", $1+$2}'`
+	for i in `seq 1 $length`;
+	do
+		Tx_bits=`echo 8 ${tx_array[count]}| awk '{printf "%.3f \n", $1*$2}'`
+		Rx_bits=`echo 8 ${rx_array[count]}| awk '{printf "%.3f \n", $1*$2}'`
 
-Total_data=`format_bites $Total_bits`
-conn_info=${conn_array[count]}
-if [ "x$conn_info" == "x" ]
-then
-conn_info=","
-fi
+		Tx_data=`format_bites $Tx_bits`
+		Rx_data=`format_bites $Rx_bits`
+		Total_bits=`echo $Tx_bits $Rx_bits| awk '{printf "%.3f \n", $1+$2}'`
 
-echo $Rx_data,$Tx_data,$Total_data,,$conn_info,,$count,${cpu_array[count]}>> $output_file
-rx_sum=`echo $rx_sum ${rx_array[count]}| awk '{printf "%.3f \n", $1+$2}'`
-tx_sum=`echo $tx_sum ${tx_array[count]}| awk '{printf "%.3f \n", $1+$2}'`
-((count++))
-done
-sum=`echo $tx_sum $rx_sum| awk '{printf "%.3f \n", $1+$2}'`
-avg=`python -c "print $sum*8/($length*1000*1000)"`
-echo $number_of_connections - $avg
-rm -rf $rx_array_file $tx_array_file $cpu_vmstat_array_file.log
- 
+		Total_data=`format_bites $Total_bits`
+		conn_info=${conn_array[count]}
+		if [ "x$conn_info" == "x" ]
+		then
+			conn_info=","
+		fi
+
+		echo $Rx_data,$Tx_data,$Total_data,,$conn_info,,$count,${cpu_array[count]}>> $output_file
+		rx_sum=`echo $rx_sum ${rx_array[count]}| awk '{printf "%.3f \n", $1+$2}'`
+		tx_sum=`echo $tx_sum ${tx_array[count]}| awk '{printf "%.3f \n", $1+$2}'`
+		((count++))
+	done
+	sum=`echo $tx_sum $rx_sum| awk '{printf "%.3f \n", $1+$2}'`
+	avg=`python -c "print $sum*8/($length*1000*1000)"`
+	echo $number_of_connections - $avg
+	rm -rf $rx_array_file $tx_array_file $cpu_vmstat_array_file.log
 }
 
 logs_folder=$1
--- remote-scripts/sar-top.sh
+++ remote-scripts/sar-top.sh
@@ -4,28 +4,30 @@
 # Author: Srikanth M
 # Email	: v-srm@microsoft.com
 #
+
 duration=$1
 filename=$2
 username=$3
 code_path="/home/$username/code/"
+
 capture_cpu(){
-for i in $(seq 1 $duration)
-do
-	date_j=`date +"%T"`
-	top_j=`top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`
-	echo $date_j,$top_j>> $filename-top.csv
-	sleep 1
-done
+	for i in $(seq 1 $duration)
+	do
+		date_j=`date +"%T"`
+		top_j=`top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`
+		echo $date_j,$top_j>> $filename-top.csv
+		sleep 1
+	done
 }
 
 capture_connections(){
-for i in $(seq 1 $duration)
-do
-	date_i=`date +"%T"`
-	netstat_i=`netstat -natp | grep iperf | grep ESTA | wc -l`
-	echo $date_i,$netstat_i >> $filename-connections.csv
-	sleep 1
-done
+	for i in $(seq 1 $duration)
+	do
+		date_i=`date +"%T"`
+		netstat_i=`netstat -natp | grep iperf | grep ESTA | wc -l`
+		echo $date_i,$netstat_i >> $filename-connections.csv
+		sleep 1
+	done
 }
 vm_bus_ver=`modinfo hv_vmbus| grep ^version| awk '{print $2}'`
 
@@ -38,11 +40,15 @@ echo $filename-top.csv $duration $filename $filename-sar.log
 [ -f $filename-sar.log  ] && rm -rf $filename-sar.log 
 [ -f $filename-vmstat.log  ] && rm -rf $filename-vmstat.log 
 [ -f $filename-connections.csv  ] && rm -rf $filename-connections.csv
+
 echo "uname: -------------------------------------------------" > $filename-sar.log
 uname -a 2>&1 >> $filename-sar.log
 echo "LIS version: --------------------------------------------" >> $filename-sar.log
 modinfo hv_vmbus 2>&1 >> $filename-sar.log
 echo "----------------------------------------------------------" >> $filename-sar.log
+echo "Number of CPU cores" `nproc` >> $filename-sar.log
+echo "Memory" `free -h| grep Mem| awk '{print $2}'` >> $filename-sar.log
+echo "Host Build Version" `dmesg | grep "Host Build" | sed "s/.*Host Build://"| awk '{print  $1}'| sed "s/;//"`  >> $filename-sar.log
 
 sar -n DEV 1 $duration 2>&1 >> $filename-sar.log&
 sar_pid=$!
