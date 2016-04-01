248afd091b8ed79fb17853edcc7c34895fa21251
SRIKKANTH <m.srikanth.m@gmail.com>
2016-04-01 22:36:04 +0530

    Added VM properties collection to Netperf server script, indentation changes

--- remote-scripts/server_start.sh
+++ remote-scripts/server_start.sh
@@ -16,30 +16,44 @@ fi
 code_path="/home/$username/code/"
 . $code_path/azuremodules.sh
 
-install_package iperf3
+if [[ `which iperf3` == "" ]]
+then
+    echo "iperf3 not installed\n Installing now..." 
+    install_package "iperf3" 
+fi
 
 for port_number in `seq 8001 8101`
 do
-iperf3 -s -D -p $port_number
+	iperf3 -s -D -p $port_number
 done
 
 while [ `netstat -natp | grep iperf | grep ESTA | wc -l` -eq 0 ]
 do
-sleep 1
-echo "waiting..."
+	sleep 1
+	echo "waiting..."
 done
 
 duration=600
 for number_of_connections  in 1 2 4 8 16 32 64 128 256 512 1024 2000 3000 4000 5000 6000
 do
-for port_number in `seq 8001 8501`
-do
-iperf3 -s -D -p $port_number
-done
-bash $code_path/sar-top.sh $duration $number_of_connections $username&
-sleep $(($duration+10))
+	for port_number in `seq 8001 8501`
+	do
+		iperf3 -s -D -p $port_number
+	done
+	bash $code_path/sar-top.sh $duration $number_of_connections $username&
+	sleep $(($duration+10))
 done
 
-vm_bus_ver=`modinfo hv_vmbus| grep ^version| awk '{print $2}'`
-logs_dir=logs-`hostname`-`uname -r`-$vm_bus_ver/
+logs_dir=logs-`hostname`-`uname -r`-`get_lis_version`/
+
+output_file=$code_path/$logs_dir/"VM_properties.csv"
+echo "" > $output_file
+echo ",OS type,"`detect_linux_ditribution`-`detect_linux_ditribution_version` >> $output_file
+echo ",Kernel version,"`uname -r` >> $output_file
+echo ",Total CPU cores,"`nproc` >> $output_file
+echo ",Memory,"`free -h| grep Mem| awk '{print $2}'`  >> $output_file
+echo ",LIS Version,"`get_lis_version` >> $output_file
+echo ",Host Version,"`get_host_version` >> $output_file
+echo "" >> $output_file
+
 bash $code_path/generate_csvs.sh $code_path/$logs_dir
