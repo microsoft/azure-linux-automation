892ed8942a91a49c2c245949765dc9f79fd4a041
Shital Savekar <iamshital@live.in>
2016-06-16 13:47:25 +0530

    Fixed root password setup script.

--- remote-scripts/enableRoot.sh
+++ remote-scripts/enableRoot.sh
@@ -14,7 +14,7 @@ sshd_configFilePath="/etc/ssh/sshd_config"
 sshdServiceName="sshd"
 usermod --password $(echo $password | openssl passwd -1 -stdin) root
 if [ $? == 0 ]; then
-    sed -i 's/PermitRootLogin/#PermitRootLogin/g' $sshd_configFilePath
+    sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' $sshd_configFilePath
     if [ $? == 0 ]; then
         echo "$sshd_configFilePath verifed for root login."
         echo "ROOT_PASSWRD_SET"
