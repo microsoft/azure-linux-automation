08fe785cd0bee08beba3b7182350e271e211d603
Nidylei <v-feile@microsoft.com>
2016-08-12 14:52:33 +0800

    remove mirror test scripts

--- remote-scripts/ICA-MIRROR-TEST-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -sSL http://mirror.azure.cn/repo/install-docker-engine.sh | sh > ICA-MIRROR-TEST-1.sh.log
-if [[ $? == 0 ]]; then
-        echo 'PASS' > Summary.log
-else
-        echo 'FAIL' > Summary.log
-fi
-echo 'TestCompleted' > state.txt
--- remote-scripts/ICA-MIRROR-TEST-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -sSL https://get.docker.com | sh > ICA-MIRROR-TEST-2.sh.log
-if [[ $? == 0 ]]; then
-        echo 'PASS' > Summary.log
-else
-        echo 'FAIL' > Summary.log
-fi
-echo 'TestCompleted' > state.txt
