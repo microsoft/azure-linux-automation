c00c489dc5605d48aef40fc8e3316d3339fefe79
v-sirebb <rm.sivakanth@gmail.com>
2016-11-03 10:42:50 +0000

    Updated the UDP net perf test script to run test with given buffer sizes and upload the data loss in database

--- remote-scripts/generate_csvs.sh
+++ remote-scripts/generate_csvs.sh
@@ -88,8 +88,10 @@ gen_csv(){
 }
 
 logs_folder=$1
+testtype=$2
+buffersize=$3
 #summary_file=$logs_folder/summary_file_`hostname`.csv
-summary_file=$logs_folder/summary_file_`hostname`.csv
+summary_file=$logs_folder/summary_file_`hostname`_${testtype}_${buffersize}.csv
 echo "" > $summary_file
 echo "Connections,Avg Throughput,Total packets,Avg CPU" > $summary_file
 for number_of_connections in 1 2 4 8 16 32 64 128 256 512 1024
