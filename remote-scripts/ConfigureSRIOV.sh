67de80b1fc65466302fe5a7c35dd71b37124e481
Shital Savekar <iamshital@users.noreply.github.com>
2017-07-24 19:13:27 +0530

    SRIOV update.
    Skipping SRIOV configuration if it is already configured by custom kernel.

--- remote-scripts/ConfigureSRIOV.sh
+++ remote-scripts/ConfigureSRIOV.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 #Reference:  https://docs.microsoft.com/en-us/azure/virtual-network/virtual-network-create-vm-accelerated-networking
-
-wget https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/plain/tools/hv/bondvf.sh
-chmod +x ./bondvf.sh
-./bondvf.sh
-cp bondvf.sh /etc/init.d
-update-rc.d bondvf.sh defaults
+bootLogs=`dmesg`
+if [[ $bootLogs =~ "Data path switched to VF" ]];
+then
+	echo "Data path switched to VF. No configuration required."
+else
+	wget https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/plain/tools/hv/bondvf.sh
+	chmod +x ./bondvf.sh
+	./bondvf.sh
+	cp bondvf.sh /etc/init.d
+	update-rc.d bondvf.sh defaults
+fi
 exit 0
