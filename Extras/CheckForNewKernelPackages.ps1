dcdb8c4e150d8ce7cd3f55b17a51919468762d70
Shital Savekar <iamshital@users.noreply.github.com>
2017-12-31 18:13:00 -0800

    Suppressed less important error messages.

--- Extras/CheckForNewKernelPackages.ps1
+++ Extras/CheckForNewKernelPackages.ps1
@@ -1,14 +1,14 @@
 param(
     $directoryPath=".\scripts\package_building"
 )
-$debPackage = (Get-ChildItem -Path $directoryPath -Recurse | Where-Object { ($_.Name).EndsWith("amd64.deb") -and ($_.Name -inotmatch "-dbg_") -and ($_.Name -imatch "linux-image-") })
+$debPackage = (Get-ChildItem -Path $directoryPath -Recurse -ErrorAction SilentlyContinue | Where-Object { ($_.Name).EndsWith("amd64.deb") -and ($_.Name -inotmatch "-dbg_") -and ($_.Name -imatch "linux-image-") })
 if ($debPackage.Count -eq 1)
 {
     Write-Host "$($debPackage.FullName) -> .\testKernel.deb"
     Copy-Item -Path $debPackage.FullName -Destination .\testKernel.deb  -Force -Verbose
 }
 
-$rpmPackage = (Get-ChildItem -Path $directoryPath -Recurse | Where-Object { ($_.Name).EndsWith("x86_64.rpm") -and ($_.Name -inotmatch "devel")  -and ($_.Name -inotmatch "headers") -and ($_.Name -imatch "kernel-") })
+$rpmPackage = (Get-ChildItem -Path $directoryPath -Recurse -ErrorAction SilentlyContinue | Where-Object { ($_.Name).EndsWith("x86_64.rpm") -and ($_.Name -inotmatch "devel")  -and ($_.Name -inotmatch "headers") -and ($_.Name -imatch "kernel-") })
 if ($rpmPackage.Count -eq 1)
 {
     Write-Host "$($rpmPackage.FullName) -> .\testKernel.rpm"
